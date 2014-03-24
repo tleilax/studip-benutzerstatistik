6c2218837a15f65ea50a9fafc485ed80716a7a83
Jan-Hendrik Willms <tleilax+github@gmail.com>
2014-03-24 15:45:45 +0100

    fix urlhelper related bug

--- assets/injector.js
+++ assets/injector.js
@@ -2,12 +2,13 @@ var urls=<?= json_encode(array_flip($urls)) ?>;
 $('a[href]:not(.no-track)').live('click',function(){
     var href = $(this).attr('href'),
         id = urls[href],
-        url = STUDIP.URLHelper.getURL('plugins.php/benutzerstatistik/tracker/url');
+        url;
     if (typeof id !== 'undefined') {
+        url = STUDIP.URLHelper.getURL('plugins.php/benutzerstatistik/tracker/url/' + id);
         if ($(this).attr('target') === '_blank') {
-            window.open(url + '/' + id);
+            window.open(url);
         } else {
-            location.href = url + '/' + id;
+            location.href = url;
         }
         return false;
     }
